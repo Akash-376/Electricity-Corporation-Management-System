69311d7960be3812de65ff58dce870d4c02cd9d1
Akash-376 <akchauhan376@gmail.com>
2023-02-24 22:40:20 +0530

    Day_4 task completed

--- Power_house/src/com/powerhouse/ui/AdminUI.java
+++ Power_house/src/com/powerhouse/ui/AdminUI.java
@@ -1,4 +1,5 @@
 package com.powerhouse.ui;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Scanner;
 
@@ -47,7 +48,10 @@ public class AdminUI {
 	public void viewAllConsumers() {
 		try {
 			List<Consumer> list = consumerDAO.viewAllConsumers();
-			list.forEach(System.out::print);
+			System.out.println("\n                                        All Consumers list");
+			System.out.println("******************************************************************************************************************");
+			list.forEach(System.out::println);
+			System.out.println("\n******************************************************************************************************************");
 		} catch (Exception e) {
 			System.out.println(e);
 		}
@@ -74,12 +78,37 @@ public class AdminUI {
 		System.out.println("Please enter consumer ID to see bill");
 		int conId = scanner.nextInt();
 		Bills bills = billsDAO.getBillByConsumerId(conId);
+		System.out.println("\n                 Consumer name: "+bills.getConsumer().getName()+"");
+		System.out.println("*******************************************************");
 		System.out.println(bills);
+		System.out.println("*******************************************************");
 	}
 	
 	
 	public void viewAllBills() throws NoRecordFoundException, SomethingWentWrongException {
 		List <Bills> list = billsDAO.viewAllBills();
-		list.forEach(System.out::println);
+		System.out.println(" \n                                                                    Power house (Uttar Pradesh)");
+		System.out.println("\n Date: "+LocalDate.now()+"                                                          Bill list");
+		System.out.println("*************************************************************************************************************************************************************************");
+		
+		for(Bills bill : list) {
+			System.out.println("Bill Id: "+ bill.getBill_id()
+					+ ", Consumer Id: " + bill.getCon_id()
+					+ ", Name: " + bill.getConsumer().getName()
+					+ ", ConsUnits: " + bill.getMeterReading()
+					+ ", Gross bill: " + bill.getMeterReading()*10
+					+ ", Tax: " + (bill.getMeterReading()*10)/100
+					+ ", Payable Amt: " + bill.getPayableAmt() 
+					+ ", Bill Status: " + bill.getBillStatus()
+					+ ", Bill date: " + bill.getBillDate()
+					+(bill.getPaymentDate() != null ? (", Payment Date: " + bill.getPaymentDate()): "")
+					+ "\n"
+					);
+		}
+		
+		System.out.println("\n*************************************************************************************************************************************************************************\n");
+		
 	}
+	
+	
 }
